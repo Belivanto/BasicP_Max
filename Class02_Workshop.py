3cc14900ddffa4db85340ad8422ceb141a32aa77
Phurin Ploysila <187945897+Belivanto@users.noreply.github.com>
2025-07-23 09:37:34 +0700

    แก้ตัวแปร x เป็น distance เพิ่ม km

--- Class02_Workshop.py
+++ Class02_Workshop.py
@@ -1,16 +1,16 @@
 #Workshop
 
-x=int(input("กรอกระยะทาง : "))
-if x <5:
-    print(x,"เดินเองเถอะ")
-elif x <=50 :
-    print(x,"ราคา 10 บาท ")
-elif x <=100 :
-    print(x, "ราคา 15 บาท")
-elif x <=300 :
-    print(x, "ราคา 25 บาท")
-elif x <=500 :
-    print(x, "ราคา 35 บาท")
+distance=int(input("กรอกระยะทาง : "))
+if distance <5:
+    print(distance,"km","เดินเองเถอะ")
+elif distance <=50 :
+    print(distance,"km","ราคา 10 บาท ")
+elif distance <=100 :
+    print(distance,"km", "ราคา 15 บาท")
+elif distance <=300 :
+    print(distance,"km", "ราคา 25 บาท")
+elif distance <=500 :
+    print(distance,"km", "ราคา 35 บาท")
 else :
     print("ราคา 45 บาท")
 
